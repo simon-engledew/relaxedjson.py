e7c431e53697692e196fd519040937537f8e1a09
Simon Engledew <simon.engledew@osirium.com>
2016-12-19 09:00:05 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="relaxedjson",
-    version="1.0.0",
+    version="1.0.1",
     author="Simon Engledew",
     author_email="simon.engledew@gmail.com",
     description=(
